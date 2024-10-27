6459e79be840643ab5c9cb7b93f4d4c7078acc82
Atharva-3000 <deshmukhsambhajirao9@gmail.com>
2024-10-27 16:45:47 +0530

    added conversions in GO

--- 03userInput/main.go
+++ 03userInput/main.go
@@ -10,11 +10,11 @@ func main() {
 	welcome := "Welcome to the user input program"
 	fmt.Println(welcome)
 
-	reader:= bufio.NewReader(os.Stdin)
-	fmt.Println("Enter the rating for my GitHub: ");
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for my GitHub: ")
 
 	// comma ok aka error ok syntax
-	input, _ := reader.ReadString('\n');
-	fmt.Println("Thanks for rating me:",input);
-	fmt.Printf("Type for rating is %T",input);
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating me:", input)
+	fmt.Printf("Type for rating is %T", input)
 }

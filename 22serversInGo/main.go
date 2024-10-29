2a83165cc701a66ea86df64fa60c1ad4168ed988
Atharva-3000 <deshmukhsambhajirao9@gmail.com>
2024-10-29 19:22:40 +0530

    Add GET request handling and response processing in Go

--- 22serversInGo/main.go
+++ 22serversInGo/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Servers in Go")
+	PerformGetRequest()
+}
+
+func PerformGetRequest() {
+	const myurl = "http://localhost:3000/get"
+
+	response, err := http.Get(myurl)
+	handleErr(err)
+	defer response.Body.Close()
+	fmt.Println("Status code: ", response.StatusCode)
+	fmt.Println("Content Length: ", response.ContentLength)
+
+	var responseString strings.Builder
+	content, _ := io.ReadAll(response.Body)
+	// fmt.Println(string(content))
+	responseString.Write(content)
+	fmt.Println(responseString.String())
+}
+
+func handleErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
